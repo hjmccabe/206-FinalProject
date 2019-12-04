b21e13359a2de158c2e8d1a2207109e6a03fc617
hjmccabe <hjmccabe@umich.edu>
2019-12-04 14:37:11 -0500

    Skeleton of data gathering file

---
+++ data_fetching.py
@@ -0,0 +1,24 @@
+import unittest
+import sqlite3
+import json
+import requests
+import os
+
+def omdb_api():
+    pass
+
+def itunes_api():
+    pass
+
+def tmdb_api():
+    pass
+
+class TestAllMethods(unittest.TestCase):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity = 2)
