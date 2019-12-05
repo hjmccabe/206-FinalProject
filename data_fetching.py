1cc38b0e3809d121ee520a8ddf82f30dfa998616
hjmccabe <hjmccabe@umich.edu>
2019-12-04 20:43:16 -0500

    Added a baseline of how to get the 20 most popular movies from TMDB. Caching next...

--- data_fetching.py
+++ data_fetching.py
@@ -11,7 +11,16 @@ def itunes_api():
     pass
 
 def tmdb_api():
-    api_key = "b45e2b59312812bca0659be8b753a532"
+    tmdb_key = "b45e2b59312812bca0659be8b753a532"
+
+    # Get 20 most popular movies from TMDB
+    popular_url = "https://api.themoviedb.org/3/movie/popular?api_key=b45e2b59312812bca0659be8b753a532&language=en-US&page=1"
+    r2 = requests.get(popular_url)
+    pop_api = json.loads(r2.text)
+    print(pop_api)
+    pop_results = pop_api["results"] #The actual movies
+    print(len(pop_results))
+
 
 class TestAllMethods(unittest.TestCase):
     def test_omdb_api():
@@ -22,7 +31,7 @@ class TestAllMethods(unittest.TestCase):
     def test_tmdb_api():
         pass
 def main():
-    tmdb_api
+    tmdb_api()
 
 if __name__ == "__main__":
     main()
