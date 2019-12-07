54b1ba17e46092c10b723b62b78deb7e3261686c
hjmccabe <hjmccabe@umich.edu>
2019-12-07 18:55:58 -0500

    Added ability for program to add to database

--- tmdb_api.py
+++ tmdb_api.py
@@ -2,6 +2,7 @@ import unittest
 import json
 import requests
 import os
+import sqlite3
 
 def get_api_data_popular():
     # Get 20 most popular movies from TMDB
@@ -31,15 +32,65 @@ def create_cache(pop_results):
     with open (cache_file, 'w') as outfile:
         json.dump(movie_results, outfile, indent = 2)
 
+def load_from_cache():
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    cache_file = dir_path + '/' + "popular_movies.json"
+    with open (cache_file, 'r') as infile:
+        st = infile.read()
+        dic = json.loads(st)
+        results = dic.get("results")
+    
+    return results
+
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def setUpPopularityTable(results, cur, conn):
+    titles = [] # We don't want any repeat titles
+
+    cur.execute("DROP TABLE IF EXISTS Popularity")
+    cur.execute("CREATE TABLE Popularity (title TEXT PRIMARY KEY, release_date DATE, popularity REAL)")
+    for movie in results:
+        title = movie.get("title")
+
+        if title not in titles:
+            titles.append(title)
+            release_date = movie.get('release_date')
+            pop = movie.get("popularity")
+            cur.execute("INSERT INTO Popularity (title, release_date, popularity) VALUES (?,?,?)",(title, release_date, pop))
+            
+    conn.commit()
+
 class TestAllMethods(unittest.TestCase):
-    def test_get_api_data_popular(self):
-        results_dict = get_api_data_popular()
-        self.assertEqual(len(results_dict), 100) # Testing if 100 movies are added
-        self.assertFalse(results_dict[0] == results_dict[20]) # Testing if unique pages were returned
+    # def test_get_api_data_popular(self):
+    #     results_dict = get_api_data_popular()
+    #     self.assertEqual(len(results_dict), 100) # Testing if 100 movies are added
+    #     self.assertFalse(results_dict[0] == results_dict[20]) # Testing if unique pages were returned
+
+    def load_from_cache(self):
+        tmdb_cache = load_from_cache()
+        self.assertEqual(len(tmdb_cache), 100)
+        self.assertEqual(tmdb_cache[0]['title'], "Frozen II")
 
 def main():
-    results = get_api_data_popular()
-    create_cache(results)
+    ### Only ran these to get popular movies once
+    ### Popular movies are dynamic and change with time
+    ### Made one cache so data is consistent
+
+    # results = get_api_data_popular()
+    # create_cache(results)
+
+    results = load_from_cache()
+    db_name = "movies.db"
+    cur, conn = setUpDatabase(db_name)
+    setUpPopularityTable(results, cur, conn)
+
+
+    
 
 if __name__ == "__main__":
     main()
