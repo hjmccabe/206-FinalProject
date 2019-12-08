2c6580b7c3aed455ebb87e5009db005d26dc8444
Madeline Rosenberg <mhrosen@umich.edu>
2019-12-07 22:42:34 -0500

    plan for comparing database to itunes data

---
+++ File4GettingDB4Itunes.py
@@ -0,0 +1,7 @@
+#will use movies.db, leftjoin Popularity and OMDB tables by title
+#will then get title, release date, and director information
+#must modify director info to .split() by ','
+#must then say if the length is >1 and <3, then if director 1 and director 2 match
+#if length >=3 then find the first 3 directors
+#if the director is N/A then search without the director in itunes
+
