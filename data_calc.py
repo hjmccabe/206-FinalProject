243c9bafaf675b347411c59294eb47f1f880ccec
hjmccabe <hjmccabe@umich.edu>
2019-12-09 00:11:52 -0500

    The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.

--- data_calc.py
+++ data_calc.py
@@ -168,6 +168,108 @@ def popularity_pie_chart(decade_dict):
     fig1.savefig("pop_movies_pie.png")
     mpl.show()
 
+def boxoffice_by_rating(f):
+    # Opening cache
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    cache_file = dir_path + '/' + "omdb.json"
+    with open (cache_file, 'r') as infile:
+        st = infile.read()
+        dic = json.loads(st)
+        results = dic.get("results") #a list of lists
+    
+    # Getting average box office per rating
+    total_dict = {}
+    count80 = 0
+    bo80 = 0
+    avg80 = 0
+    count60 = 0
+    bo60 = 0
+    avg60 = 0
+    count40 = 0
+    bo40 = 0
+    avg40 = 0
+    count20 = 0
+    bo20 = 0
+    avg20 = 0
+    mvsucks = 0
+    bomvsucks = 0 
+    sucksavg = 0 
+    for movie in results:
+        boxoffice = movie[1]
+        rating = movie[-1]
+        if boxoffice != 0 and rating !=0:
+            if rating >80:
+                count80+= 1
+                bo80+=boxoffice
+            if rating >60:
+                count60+= 1
+                bo60+=boxoffice
+            if rating>40:
+                count40+= 1
+                bo40+=boxoffice
+            if rating>20:
+                count20+= 1
+                bo20+=boxoffice
+            else:
+                mvsucks+=1
+                bomvsucks+=1
+    avg80 = bo80/count80
+    avg60 = bo60/count60
+    avg40 = bo40/count40
+    avg20 = bo20/count20
+    if mvsucks !=0:
+        sucksavg = bomvsucks/mvsucks
+
+    # Add to text file
+    f.write("------\n")
+    f.write("Average Boxoffice Price per Rating Category\n")
+    f.write("rating category, number of movies category, average boxoffice\n")
+    entry1 = "80-100, {}, {}\n".format(str(count80), str(avg80))
+    f.write(entry1)
+    entry2 = "60-80, {}, {}\n".format(str(count60),str(avg60)) 
+    f.write(entry2)
+    entry3 = "40-60, {}, {}\n".format(str(count40), str(avg40))
+    f.write(entry3)
+    entry4 = "20-40, {}, {}\n".format(str(count20), str(avg20))
+    f.write(entry4)
+    if sucksavg !=0:
+        entry5 = "<=20, {}, {}\n".format(str(mvsucks), str(sucksavg))
+        f.write(entry5)
+    else:
+        entry5 = "<=20, 0, 0\n".format(str(mvsucks), str(sucksavg))
+        f.write(entry5)
+    f.write('\n')
+
+def make_visualizations(file):
+    rtrating = []
+    bins = []
+    count = -20
+    num = 3
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_ = dir_path + '/' + "calculations.txt"
+    with open (file_, 'r') as infile:
+        something = infile.readlines()#[31:]
+    for line in something: 
+        if num in range(len(something) - 1) and line == something[num]:
+            num+=1
+            line = line.strip('\n')
+            count+=20
+            bins.append(count)
+            l = line.split(',')
+            rtrating.append(int(l[1]))
+    bins.append(100)
+    mpl.hist(rtrating, bins, histtype='bar', rwidth=0.8)
+    mpl.xticks(rotation=90)
+    mpl.tight_layout()
+    mpl.title("Average Boxoffice Price per Rating Category")
+    mpl.xlabel("rating category")
+    mpl.ylabel("number of movies category")
+    fig, ax = mpl.subplots()
+    fig.savefig("omdbhist.png")
+    mpl.show()
+    mpl.legend()
+
+
 def main():
     with open("calculations.txt", 'w') as f:
         sorted_average_popularity = popularity_by_year(f)
@@ -175,7 +277,9 @@ def main():
 
         pop_decade = popularity_by_decade(sorted_average_popularity, f)
         popularity_pie_chart(pop_decade)
-        
+
+        boxoffice_by_rating(f)
+        make_visualizations(f)
 
 
 if __name__ == "__main__":
