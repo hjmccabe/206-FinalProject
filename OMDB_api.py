c864dbe899bf92d60602fbc2e506cef745e1aded
hjmccabe <hjmccabe@umich.edu>
2019-12-14 13:37:34 -0500

    Made it so no changes by hand need to be made

--- OMDB_api.py
+++ OMDB_api.py
@@ -19,7 +19,6 @@ def get_popular_movie_titles():
 
     titles_no_repeat = []
     
-
     for movie in titles:
         if movie not in titles_no_repeat:
             titles_no_repeat.append(movie)
@@ -54,34 +53,6 @@ def get_api_data(film):
     movie_info = (movie_name, box_office, director, rotten_tomato)
 
     return movie_info
-    
-    # Each fetch returns 1 movie per page
-    # want 20 movies
-
-    # popular_movie_titles = get_popular_movie_titles()
-    # print(popular_movie_titles)
-
-    # for title in popular_movie_titles: 
-    #     ratings_url = baseurl.format(title, api_key)
-    #     r = requests.get(ratings_url)
-    #     data = json.loads(r.text)
-    #     box_office = data.get("BoxOffice")
-    #     try:
-    #         box_office = int(box_office[1:].replace(',', ''))
-    #     except:
-    #         box_office = 0
-    #     movie_name = data.get("Title")
-    #     director = data.get("Director")
-    #     ratings = data.get("Ratings")
-    #     try:
-    #         rotten_tomato = ratings[1].get("Value")
-    #         rotten_tomato = int(rotten_tomato[:-1])
-    #     except:
-    #         rotten_tomato = 0
-
-    #     movie_info.append((movie_name, box_office, director, rotten_tomato))
-
-    # return movie_info
 
 def make_cache(movie_info):
     # Cache was created on 12/7/19 at 9:45 PM
@@ -94,7 +65,6 @@ def make_cache(movie_info):
 
     with open (cache_file, 'w') as outfile:
         json.dump(movie_results, outfile, indent = 2)
-    pass
 
 def open_cache():
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -122,9 +92,6 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def directors_table(movie, cur, conn):
-    # cur.execute("DROP TABLE IF EXISTS Directors")
-    # cur.execute("CREATE TABLE Directors (title TEXT PRIMARY KEY, director TEXT)")
-
     # Movie info: (Title, box office, director, rotten tomatoes)
     cur.execute("INSERT INTO Directors (title, director) VALUES (?,?)", (movie[0], movie[2]))
     conn.commit()
@@ -133,10 +100,6 @@ def bort_table(movie, cur, conn):
     # Excuse me, my son is also named 'Bort'
     # We need more Bort license plates in the Gift Shop. Repeat, we are sold out of Bort license plates.
 
-
-    # cur.execute("DROP TABLE IF EXISTS Ratings_and_Box")
-    # cur.execute("CREATE TABLE Ratings_and_Box (title TEXT PRIMARY KEY, box_office INT, rotten_tomatoes INT)")
-
     # Movie info: (Title, box office, director, rotten tomatoes)
     cur.execute("INSERT INTO Ratings_and_Box  (title, box_office, rotten_tomatoes) VALUES (?,?,?)",(movie[0], movie[1], movie[3]))
     conn.commit()
@@ -144,38 +107,36 @@ def bort_table(movie, cur, conn):
 def main():
     cur, conn = setUpDatabase("movies_data.db")
     titles = get_popular_movie_titles()
-    i = 0
     movie_info_list = []
 
-    ### Only run this if not pulling from API (running from cache) ###
-    ### The instructions apply until "STOP 1" ###
-    # movie_info_list = open_cache()
-    # for film in movie_info_list:
-    #     directors_table(film, cur, conn)
-    #     bort_table(film, cur, conn)
+    source = input("Do you want to get data from the cache or the API? (type 'cache', 'api', or 'quit')").lower()
+    api_source = source == "api"
+    cache_source = source == "cache"
+    quit_source = source == 'quit'
 
-    ### STOP 1###
+    while((api_source == False) and (cache_source == False) and (quit_source == False)):
+        source = input("Please type either type 'cache', 'api', or 'quit')").lower()
+        api_source = source == "api"
+        cache_source = source == "cache"
+        quit_source = source == 'quit'
 
-     ### Only run these if you need to use API ###
-     ### The instructions apply until "STOP 2" ###
-    for film in titles:
-       
-        movie = get_api_data(film)
-        movie_info_list.append(movie)
-        i+=1
-        print(i)
+    if cache_source == True:
+        movie_info_list = open_cache()
+        for film in movie_info_list:
+            directors_table(film, cur, conn)
+            bort_table(film, cur, conn)
 
-        print(movie)
+        conn.close()
 
-        directors_table(movie, cur, conn)
-        bort_table(movie, cur, conn)
-    
-    make_cache(movie_info_list)
+    if api_source == True:
+        for film in titles:
+            movie = get_api_data(film)
+            movie_info_list.append(movie)
+            directors_table(movie, cur, conn)
+            bort_table(movie, cur, conn)
 
-    ### STOP 2###
-    conn.close()
-
-
+        make_cache(movie_info_list)
+        conn.close()
 
 if __name__ == "__main__":
     main()
